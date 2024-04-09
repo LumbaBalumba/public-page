a7552ea23d0bc0904e5fd958d89328c2258dcfa5
LumbaBalumba <vlad.lazar.ik528@yandex.ru>
2024-04-09 22:10:43 +0300

    rewrite everything to rust lol

--- site/go.mod
+++
@@ -1,3 +0,0 @@
-module public-page
-
-go 1.22.1
---
+++ site/src/main.rs
@@ -0,0 +1,24 @@
+use std::{fs, io::Result};
+
+use actix_files::Files;
+use actix_web::{get, http::header::ContentType, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn index() -> impl Responder {
+    let body = fs::read_to_string("templates/index.html").unwrap();
+    HttpResponse::Ok()
+        .content_type(ContentType::html())
+        .body(body)
+}
+
+#[actix_web::main]
+async fn main() -> Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(Files::new("/static", "static/").show_files_listing())
+            .service(index)
+    })
+    .bind("0.0.0.0:8080")?
+    .run()
+    .await
+}
