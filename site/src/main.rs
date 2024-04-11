5f419e1d46b14e7c6f19f1cbdc5fe81d5a99ce91
LumbaBalumba <vlad.lazar.ik528@yandex.ru>
2024-04-12 00:23:41 +0300

    add example sites

--- site/src/main.rs
+++ site/src/main.rs
@@ -11,12 +11,21 @@ async fn index() -> impl Responder {
         .body(body)
 }
 
+#[get("/sites")]
+async fn sites() -> impl Responder {
+    let body = fs::read_to_string("templates/sites.html").unwrap();
+    HttpResponse::Ok()
+        .content_type(ContentType::html())
+        .body(body)
+}
+
 #[actix_web::main]
 async fn main() -> Result<()> {
     HttpServer::new(|| {
         App::new()
             .service(Files::new("/static", "static/").show_files_listing())
             .service(index)
+            .service(sites)
     })
     .bind("0.0.0.0:8080")?
     .run()
