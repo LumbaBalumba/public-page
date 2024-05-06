31d0f0284d79768b1adf4226c925634837dcc0a1
LumbaBalumba <vlad.lazar.ik528@yandex.ru>
2024-05-06 22:10:41 +0300

    move to rocket

--- site/src/main.rs
+++ site/src/main.rs
@@ -1,39 +1,21 @@
-use std::{fs, io::Result};
-
-use actix_files::Files;
-use actix_web::{get, http::header::ContentType, App, HttpResponse, HttpServer, Responder};
+use rocket::{
+    fs::{relative, FileServer, NamedFile},
+    get, launch, routes,
+};
 
 #[get("/")]
-async fn index() -> impl Responder {
-    let body = fs::read_to_string("templates/index.html").unwrap();
-    HttpResponse::Ok()
-        .content_type(ContentType::html())
-        .body(body)
-}
-
-#[get("/sites")]
-async fn sites() -> impl Responder {
-    let body = fs::read_to_string("templates/sites.html").unwrap();
-    HttpResponse::Ok()
-        .content_type(ContentType::html())
-        .body(body)
+async fn index() -> Option<NamedFile> {
+    NamedFile::open("templates/index.html").await.ok()
 }
 
 #[get("/favicon.ico")]
-async fn favicon() -> actix_web::Result<actix_files::NamedFile> {
-    Ok(actix_files::NamedFile::open("static/img/favicon.ico")?)
+async fn favicon() -> Option<NamedFile> {
+    NamedFile::open("static/img/favicon.ico").await.ok()
 }
 
-#[actix_web::main]
-async fn main() -> Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(Files::new("/static", "static/").show_files_listing())
-            .service(index)
-            .service(sites)
-            .service(favicon)
-    })
-    .bind("0.0.0.0:8080")?
-    .run()
-    .await
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", routes![index, favicon])
+        .mount("/static", FileServer::from(relative!("static")))
 }
