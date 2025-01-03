b021095005157d913109aaa8d0024a8bafbeb0fa
LumbaBalumba <vlad.lazar.ik528@yandex.ru>
2025-01-03 19:29:12 +0300

    auth user views

--- auth/api/views.py
+++ auth/api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from api.serializers import UserSerializer
+
+User = get_user_model()
+
+
+class UserViewSet(ModelViewSet):
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return User.objects.filter(id=self.request.user.id)
