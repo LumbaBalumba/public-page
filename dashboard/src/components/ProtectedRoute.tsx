5e9c9c95f5ec7a9f8f0036a3777ca82651fee2ce
LumbaBalumba <vlad.lazar.ik528@yandex.ru>
2025-07-10 13:16:48 +0300

    move dashboard to a separate repo

--- dashboard/src/components/ProtectedRoute.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Navigate, Outlet } from "react-router-dom";
-import { getAccessToken } from "../utils/cookies";
-
-const ProtectedRoute: React.FC = () => {
-  return getAccessToken() ? <Outlet /> : <Navigate to="/login" replace />;
-};
-
-export default ProtectedRoute;
