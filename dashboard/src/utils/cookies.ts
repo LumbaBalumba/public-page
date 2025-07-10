bb45929f6d6251c21fac0b230407620bf84779b7
LumbaBalumba <vlad.lazar.ik528@yandex.ru>
2025-07-10 13:09:05 +0300

    remove secure

--- dashboard/src/utils/cookies.ts
+++ dashboard/src/utils/cookies.ts
@@ -7,8 +7,8 @@ export const getRefreshToken = (): string | undefined =>
   Cookies.get("refresh_token");
 
 export const setTokens = (access: string, refresh: string) => {
-  Cookies.set("access_token", access, { secure: true, sameSite: "strict" });
-  Cookies.set("refresh_token", refresh, { secure: true, sameSite: "strict" });
+  Cookies.set("access_token", access, { sameSite: "strict" });
+  Cookies.set("refresh_token", refresh, { sameSite: "strict" });
 };
 
 export const clearTokens = () => {
